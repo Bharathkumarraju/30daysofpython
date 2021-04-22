f525f6d984654caa2e1a18656010c387185c8f9d
Bharath <bharath@hooq.tv>
2021-04-22 11:41:59 +0800

    update gitignore

---
+++ debug_in_pycharm/output.sh
@@ -0,0 +1,26 @@
+/usr/local/bin/python3.7 /Users/bharathdasaraju/git/30daysofpython/debug_in_pycharm/price.py
+  Things  Quantity  Total_Price
+0  Spoon     105.0         1050
+1  Plate      78.0         2250
+2    Cup      90.0         1550
+3   Fork       NaN          890
+4  Bread      21.0          620
+  Things  Quantity  Total_Price
+0  Spoon     105.0         1050
+1  Plate      78.0         2250
+2    Cup      90.0         1550
+3   Fork       0.0          890
+4  Bread      21.0          620
+10.0
+28.846153846153847
+17.22222222222222
+Traceback (most recent call last):
+  File "/Users/bharathdasaraju/git/30daysofpython/debug_in_pycharm/price.py", line 23, in <module>
+    get_price_info()
+  File "/Users/bharathdasaraju/git/30daysofpython/debug_in_pycharm/price.py", line 18, in get_price_info
+    price_of_a_unit = (total_price / quantity)
+ZeroDivisionError: float division by zero
+
+Process finished with exit code 1
+
+
