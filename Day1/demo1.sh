52b18c360f11d4be8e8beb48e2f9a931b9932975
Bharath <bharath@hooq.tv>
2021-04-27 15:33:49 +0800

    Create a list

---
+++ Day1/demo1.sh
@@ -0,0 +1,26 @@
+>>> this is a word
+  File "<stdin>", line 1
+    this is a word
+                 ^
+SyntaxError: invalid syntax
+>>> "this is  a word"
+'this is  a word'
+>>>
+
+
+>>> "hi this is another" + " string"
+'hi this is another string'
+>>>
+
+
+>>> abc + abc
+'some stringsome string'
+>>> abc +  abc
+'some stringsome string'
+>>> abc +  abc
+'some stringsome string'
+>>> abc + abc
+'some stringsome string'
+>>> abc + " hello"
+'some string hello'
+>>>
---
+++ Day2/demo1.sh
@@ -0,0 +1,23 @@
+>>> []
+[]
+>>> ['some string']
+['some string']
+>>> ['some string', ]
+['some string']
+>>> ['some string', 123]
+['some string', 123]
+
+>>> ['some string', 123, "another string"]
+['some string', 123, 'another string']
+>>>
+
+
+>>> ["some string", 123, "another string"]
+['some string', 123, 'another string']
+>>>
+
+>>> list_var = ["some string", 123, "another string"]
+>>> list_var
+['some string', 123, 'another string']
+>>>
+
