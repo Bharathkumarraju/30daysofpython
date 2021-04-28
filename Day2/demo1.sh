7edbd3a881d5071753dd7b9311c7ab805a00234b
Bharath <bharath@hooq.tv>
2021-04-28 08:04:03 +0800

    refresh python

--- Day2/demo1.sh
+++ Day2/demo1.sh
@@ -21,3 +21,25 @@
 ['some string', 123, 'another string']
 >>>
 
+
+
+
+>>> list_var.append("some other item")
+>>>
+>>> list_var
+['some string', 123, 'another string', 'some other item']
+>>> list_var.pop()
+'some other item'
+>>> list_var
+['some string', 123, 'another string']
+>>>
+
+
+>>> len(list_var)
+3
+>>> list_var
+['some string', 123, 'another string']
+>>> len("a string")
+8
+>>>
+
