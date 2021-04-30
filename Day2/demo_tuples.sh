432b1d4dfdb14e201020bcab2b071baa9fc0ece8
Bharath <bharath@hooq.tv>
2021-04-30 16:59:44 +0800

    dict of dicts

---
+++ Day2/demo_tuples.sh
@@ -0,0 +1,12 @@
+
+>>> tup = ()
+>>> type(tup)
+<class 'tuple'>
+
+>>> tup = (
+... "one", "two", ("three", "four"), ("five", "six"))
+>>> tup
+('one', 'two', ('three', 'four'), ('five', 'six'))
+>>>
+
+
