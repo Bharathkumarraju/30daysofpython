a4f51202086c851ef11c5edbb1d0f52f1e5c27e1
Bharath <bharath@hooq.tv>
2021-05-01 07:11:04 +0800

    all about python

---
+++ Day3/demo1.sh
@@ -0,0 +1,24 @@
+>>> bag = [10, 1231, 121321, 1243, 1342523, 123, 23442]
+>>> len(bag)
+7
+>>>
+>>> for item in bag:
+...     print(item)
+...
+10
+1231
+121321
+1243
+1342523
+123
+23442
+>>>
+
+
+>>> for item in bag:
+...     if item == 123:
+...         print('yeahhhhhh got it')
+...
+yeahhhhhh got it
+>>>
+
---
+++ Day3/demo2.py
@@ -0,0 +1,10 @@
+bag = [10, 1231, 121321, 1243, 1342523, 123, 23442]
+
+i = 0
+
+for item in bag:
+    i = i + 1
+    if item == 23442:
+        print('the item found on iteration of ', i)
+        print('success')
+
