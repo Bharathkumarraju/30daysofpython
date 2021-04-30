20d80ee0386488dfd11652fc4d78b6647dcefa8f
Bharath <bharath@hooq.tv>
2021-05-01 06:50:22 +0800

    all about tuples

--- Day2/demo_tuples.sh
+++ Day2/demo_tuples.sh
@@ -20,3 +20,26 @@
 'two'
 >>>
 
+>>> tup += ("yetanother", 123)
+>>> tup
+('one', 'two', ('three', 'four'), ('five', 'six'), 'yetanother', 123)
+>>>
+
+
+
+>>> tup += (("yetanother", 123),)
+>>> tup
+('one', 'two', ('three', 'four'), ('five', 'six'), 'yetanother', 123, ('yetanother', 123))
+>>>
+
+
+
+>>> the_list1=[]
+>>> the_list1
+[]
+>>> abc=["another", 123, "another"]
+>>> the_list1.append(abc)
+>>>
+>>> the_list1
+[['another', 123, 'another']]
+>>>
