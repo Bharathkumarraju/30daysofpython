340c60df85fb6cd50bc00424d9b160349baff374
Bharath <bharath@hooq.tv>
2021-05-01 08:08:26 +0800

    All about conditionals:

---
+++ Day4/demo1.sh
@@ -0,0 +1,29 @@
+bharathdasaraju@MacBook-Pro git $ python3
+Python 3.7.4 (default, Sep  7 2019, 18:27:02)
+[Clang 10.0.1 (clang-1001.0.46.4)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> True
+True
+>>> False
+False
+>>> true
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'true' is not defined
+>>> false
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'false' is not defined
+>>> print(True)
+True
+>>> print(x==x)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'x' is not defined
+>>> print(1==1)
+True
+>>> print('x'==1)
+False
+>>> print('x'=='x')
+True
+>>>
